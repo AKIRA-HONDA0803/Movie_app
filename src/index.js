21446c53b9d7c5a376a4151ec3109ef4d9ec9983
AkiraHonda <05410kira@gmail.com>
2021-11-30 10:34:43 +0900

    ビデオリストの表示・レスポンシブ

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// ブラウザのデフォルトのCSSを初期化
+import　'normalize.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { StoreProvider } from './store/index'
