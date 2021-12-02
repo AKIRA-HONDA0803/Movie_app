583660062895f3a71475e228a547446fa0e270a1
AkiraHonda <05410kira@gmail.com>
2021-12-02 09:41:34 +0900

    関連動画の表示

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import React, { createContext, useReducer } from 'react'
 
 const initialState = {
   popular: [],
+  related: [],
   selected: {}
 }
 // initialStateの中に複数のStateが含まれる場合は、必ずスプレッド構文を使って...stateを追加してから更新を行う
@@ -10,8 +11,10 @@ const reducer = (state, action) => {
   switch(action.type) {
     case 'SET_POPULAR':
       return {...state, popular: action.payload.popular }
+    case 'SET_RELATED':
+      return {...state, related: action.payload.related }
     case 'SET_SELECTED':
-      return {...state, selected: action.payload.selected}
+      return {...state, selected: action.payload.selected }
     default:
       return state
   }
