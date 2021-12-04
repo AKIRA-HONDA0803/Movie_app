c57af2bdc929ed0a2ea50ea63e11d074de0b7dc4
AkiraHonda <05410kira@gmail.com>
2021-12-04 09:34:35 +0900

    検索結果のリスト表示

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -3,6 +3,8 @@ import Layout from '../components/Layout/Layout'
 import { useLocation } from "react-router-dom"
 import { fetchSearchData } from '../apis'
 import { Store } from '../store/index'
+import VideoGrid from '../components/VideoGrid/VideoGrid'
+import VideoGridItem from '../components/VideoGridItem/VideoGridItem'
 
 const Search = () => {
   const { globalState, setGlobalState } = useContext(Store)
@@ -22,7 +24,19 @@ const Search = () => {
   },[])
   return (
     <Layout>
-      Search page
+      <VideoGrid>
+        {
+          globalState.searched ? globalState.searched.map((search) => {
+            return(
+              <VideoGridItem
+                id={search.id.videoId}
+                key={search.id.videoId}
+                src={search.snippet.thumbnails.medium.url}
+                title={search.snippet.title} />
+            )
+          }) : <span>no data</span>
+        }
+      </VideoGrid>
     </Layout>
   )
 }
